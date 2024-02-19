00bc7f200ce5c09a6362bf59544ca173a271c3fb
JohnyDevp <holan.jan@outlook.com>
2024-02-19 23:05:45 +0100

    main design getting ready

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+
+// remaining time count animation
 let counter = document.getElementById("timer");
 setInterval(() => {
   let diff = (new Date(2024, 7, 13, 1, 0, 0) - Date.now()) / 1000;
@@ -16,6 +18,7 @@ setInterval(() => {
   }`;
 }, 1000);
 
+// toggle navbar menu animation
 let isMenuOpen = false;
 function toggleMenu() {
   let navbar = document.getElementById("navbar");
@@ -27,7 +30,7 @@ function toggleMenu() {
       navbar.style.transform = "translateX(0%)";
 
       // move the menu icon too
-      menu_container.style.width = "calc(50% + 60px)";
+      menu_container.style.width = 'calc(50% + 60px)';
 
       isMenuOpen = true;
       break;
@@ -40,4 +43,16 @@ function toggleMenu() {
       isMenuOpen = false;
       break;
   }
+}
+
+// animation of selected navbar button
+function navbarButtonSelected_handler(btn_id) {
+    let current_btn = document.getElementById(btn_id)
+    let all_btns = document.getElementsByTagName('li')
+    for (let i = 0; i<all_btns.length; i++)
+    {
+        all_btns[i].classList.remove('li-active')
+    }
+
+    current_btn.classList.add('li-active')
 }

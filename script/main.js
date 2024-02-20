9645bf3479a665f562e0024ff11c429e79d87be0
JohnyDevp <holan.jan@outlook.com>
2024-02-20 11:05:36 +0100

    content creation start

--- script/main.js
+++ script/main.js
@@ -1,58 +1,83 @@
-
 // remaining time count animation
 let counter = document.getElementById("timer");
 setInterval(() => {
-  let diff = (new Date(2024, 7, 13, 1, 0, 0) - Date.now()) / 1000;
-  let days = Math.floor(diff / (60 * 60 * 24));
-  let hours = Math.floor((diff % (60 * 60 * 24)) / (60 * 60));
-  let minutes = Math.floor((diff % (60 * 60)) / 60);
-  let seconds = Math.floor(diff % 60);
-  counter.innerHTML = `${days} ${
-    days > 4 || days == 0 ? "dnů" : days > 1 ? "dny" : "den"
-  } <br> ${hours} ${
-    hours > 4 || hours == 0 ? "hodin" : hours > 1 ? "hodiny" : "hodina"
-  } <br> ${minutes} ${
-    minutes > 4 || minutes == 0 ? "minut" : minutes > 1 ? "minuty" : "minuta"
-  } <br> ${seconds} ${
-    seconds > 4 || seconds == 0 ? "vteřin" : seconds > 1 ? "vteřiny" : "vteřina"
-  }`;
+	let diff = (new Date(2024, 7, 13, 1, 0, 0) - Date.now()) / 1000;
+	let days = Math.floor(diff / (60 * 60 * 24));
+	let hours = Math.floor((diff % (60 * 60 * 24)) / (60 * 60));
+	let minutes = Math.floor((diff % (60 * 60)) / 60);
+	let seconds = Math.floor(diff % 60);
+	counter.innerHTML = `${days} ${
+		days > 4 || days == 0 ? "dnů" : days > 1 ? "dny" : "den"
+	} <br> ${hours} ${
+		hours > 4 || hours == 0 ? "hodin" : hours > 1 ? "hodiny" : "hodina"
+	} <br> ${minutes} ${
+		minutes > 4 || minutes == 0
+			? "minut"
+			: minutes > 1
+			? "minuty"
+			: "minuta"
+	} <br> ${seconds} ${
+		seconds > 4 || seconds == 0
+			? "vteřin"
+			: seconds > 1
+			? "vteřiny"
+			: "vteřina"
+	}`;
 }, 1000);
 
 // toggle navbar menu animation
 let isMenuOpen = false;
 function toggleMenu() {
-  let navbar = document.getElementById("navbar");
-  let menu_container = document.getElementById("menu-button-container");
-  let menu_icon = document.getElementById("menu-icon");
-  switch (isMenuOpen) {
-    case false:
-      // open the navbar
-      navbar.style.transform = "translateX(0%)";
+	let navbar = document.getElementById("navbar");
+	let menu_container = document.getElementById("menu-button-container");
+	let menu_icon = document.getElementById("menu-icon");
+	switch (isMenuOpen) {
+		case false:
+			// open the navbar
+			navbar.style.transform = "translateX(0%)";
 
-      // move the menu icon too
-      menu_container.style.width = 'calc(50% + 60px)';
+			// move the menu icon too
+			menu_container.style.width = "calc(50% + 60px)";
 
-      isMenuOpen = true;
-      break;
-    case true:
-      // hide the navbar
-      navbar.style.transform = "translateX(-100%)";
+			isMenuOpen = true;
+			break;
+		case true:
+			// hide the navbar
+			navbar.style.transform = "translateX(-100%)";
 
-      menu_container.style.width = "60px";
+			menu_container.style.width = "60px";
 
-      isMenuOpen = false;
-      break;
-  }
+			isMenuOpen = false;
+			break;
+	}
 }
 
 // animation of selected navbar button
 function navbarButtonSelected_handler(btn_id) {
-    let current_btn = document.getElementById(btn_id)
-    let all_btns = document.getElementsByTagName('li')
-    for (let i = 0; i<all_btns.length; i++)
-    {
-        all_btns[i].classList.remove('li-active')
-    }
+	let current_btn = document.getElementById(btn_id);
+	// get all buttons and remove the activie class from them
+	let all_btns = document.getElementsByTagName("li");
+	for (let i = 0; i < all_btns.length; i++) {
+		all_btns[i].classList.remove("li-active");
+	}
 
-    current_btn.classList.add('li-active')
+	// make active (marked) only the clicked button
+	current_btn.classList.add("li-active");
+
+	// open the div the belongs to clicked button
+	document.getElementsByClassName("content-container")[0].style.visibility =
+		"visible";
+  
+	document
+		.querySelectorAll("div.content-container>div")
+		.forEach((div_content) => {
+			if (div_content.id == String(btn_id).split("-")[0] + "-content") {
+				div_content.style.visibility = "visible";
+				div_content.style.height = 'inherit'
+			} else {
+				div_content.style.visibility = "hidden";
+				div_content.style.height = '0px'
+			}
+		});
+	document.getElementsByClassName('mainpage-body')[0].style.visibility = 'hidden'
 }
